7ddd1cc210dcede0a9835c49c356b2d0f1e44b4c
Emil <deine@email.de>
2025-05-23 16:57:38 +0200

    x add

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import rezepte from './rezepte';
 import Generator from './Generator';
 
@@ -248,7 +248,7 @@ function RezepteApp() {
                 color: '#EAEAEA'
               }}
             >
-              🗑️
+              ❌
             </button>
             <img
               src={selectedRecipe.image}
